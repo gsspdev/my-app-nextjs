b742ce4ad9eaf1255e1e70551a0d39a8481c513e
gsspdev <108854023+gsspdev@users.noreply.github.com>
2022-12-24 06:16:30 -0600

    init commit 2

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    runtime: 'experimental-edge',
+  },
   reactStrictMode: true,
 }
 
 module.exports = nextConfig
+
